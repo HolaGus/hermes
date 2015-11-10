a13cde21915439aa929e200e05efccc8dbe6dddd
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-11-09 23:32:29 -0600

    added RxJava

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,16 +1,45 @@
-import ratpack.groovy.template.MarkupTemplateModule
-
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.jsonNode
+import static ratpack.rx.RxRatpack.observe
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import ratpack.form.Form
+import ratpack.groovy.template.MarkupTemplateModule
+import ratpack.rx.RxRatpack
+import ratpack.server.Service
+import ratpack.server.StartEvent
+
+final Logger logger = LoggerFactory.getLogger(ratpack.class)
 
 ratpack {
   bindings {
     module MarkupTemplateModule
+
+    bindInstance Service, new Service() {
+      @Override
+      void onStart(StartEvent event) throws Exception {
+        logger.info "Initializing RX"
+        RxRatpack.initialize()
+      }
+    }
   }
 
   handlers {
     get {
       render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App CHIDA")
+    }
+
+    path("create") {
+      byMethod {
+        post {
+          //observe(parse(Form)).map { it.get('foo') }.subscribe { context.render(it) }
+          //parse(jsonNode()).observe()
+          //observe(parse(jsonNode())).flatMap().single()
+          observe(parse(jsonNode())).map { it.get('foo') }.subscribe { context.render(it.toString()) }
+        }
+      }
     }
 
     files { dir "public" }
